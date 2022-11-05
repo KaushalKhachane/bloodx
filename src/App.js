25374735ba897116bfa5509b414fdcfe6c30d814
knk2902 <khachanekn29@gmail.com>
2022-11-05 22:29:30 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header.js';
+import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
