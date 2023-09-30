5eaab89833b6885d6c01fc7275487e6ea3b41222
knk2902 <khachanekn29@gmail.com>
2023-09-30 16:24:28 +0530

    Added send otp for email

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,11 @@ import hospitalsRouter from "./routes/Hospitals.js";
 import bloodRequestRouter from "./routes/BloodRequests.js";
 import bloodStockRouter from "./routes/BloodStock.js";
 import adminRouter from "./routes/Admin.js";
+import { User } from "../backend/models.js";
+
+import nodemailer from "nodemailer";
+import otpGenerator from "otp-generator";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
@@ -21,16 +26,69 @@ app.use("/api/bloodrequests", bloodRequestRouter);
 app.use("/api/bloodstock", bloodStockRouter);
 app.use("/api/admin", adminRouter);
 
+let otp;
+app.post("/send-otp", async (req, res) => {
+  const { user_email, user_type } = req.body;
+  const user = await User.findOne({ user_email });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  otp = otpGenerator.generate(6, {
+    digits: true,
+    alphabets: false,
+    upperCase: false,
+    specialChars: false,
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "khachanekn29@gmail.com",
+      pass: "nzqboskwgmfaxfsw"
+    },
+  });
+
+  const mailOptions = {
+    from: "khachanekn29@gmail.com",
+    to: user_email,
+    subject: "OTP for Login to BloodX !!",
+    text: `Your OTP for login is: ${otp}`,
+  };
+
+  transporter.sendMail(mailOptions, async (error, info) => {
+    if (error) {
+      return res.status(500).json({ message: "Error sending OTP", error:error });
+    }
+
+    res.status(200).json({ message: "OTP sent successfully" });
+  });
+});
+
+app.post("/verify-otp", async (req, res) => {
+  const { user_otp } = req.body;
+  const { user_email } = req.body;
+  const user = await User.findOne({ user_email });
+
+  if (user_otp !== otp) {
+    return res.status(401).json({ message: "Invalid OTP" });
+  }
+
+  const donor_token = jwt.sign({ userId: user._id }, "my-secret-key", {});
+  localStorage.setItem("donor_token", donor_token);
+  localStorage.setItem("user_type", "donor");
+  res.status(200).json({ token });
+});
+
 
 const PORT = process.env.PORT || 8801;
 
-// Connect to MongoDB Atlas
 mongoose.connect(
   "mongodb+srv://knk29:khachane29@atlascluster.wtf8ey2.mongodb.net/blood_bank?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
   }
 );
 
--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -2,16 +2,22 @@ import express from "express";
 import { User } from "../models.js";
 
 const router = express.Router();
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 router.post("/signup", async (req, res) => {
   try {
+    const { password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       user_email: req.body.user_email,
       user_name: req.body.user_name,
       user_phone_no: req.body.user_phone_no,
       user_gender: req.body.user_gender,
       user_address: req.body.user_address,
-      user_password: req.body.user_password,
+      user_password: hashedPassword,
     });
 
     await newUser.save();
@@ -27,13 +33,25 @@ router.get("/login", async (req, res) => {
   const { user_email, user_password } = req.body;
 
   try {
-    const user = await User.findOne({ user_email, user_password });
+    const user = await User.findOne({ user_email });
 
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: "Wrong username/password !!" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    const isPasswordValid = await bcrypt.compare(
+      user_password,
+      user.user_password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, "mysecretkey");
+    res.status(200).json({ token });
+
+    
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal Server Error" });
